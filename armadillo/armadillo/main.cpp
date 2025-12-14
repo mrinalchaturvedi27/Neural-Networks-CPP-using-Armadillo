7ae74a986aef14be90374eea8f6806a1a3c09c8b
Mrinal Chaturvedi <mrinal.chaturvedi27@gmail.com>
2025-12-14 17:13:02 +0530

    Initial commit

---
+++ armadillo/armadillo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  armadillo
+//
+//  Created by Mrinal Chaturvedi on 13/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
